cdaca027d6cf174bae42055f7b44a37da0552cb2
Anton Maurer <developer@maureranton.com>
2026-05-22 05:11:45 +0100

    rebrand: Element→Progressive Chat in strings + license headers

--- vector/src/main/java/im/vector/app/features/powerlevel/Role.kt
+++ vector/src/main/java/im/vector/app/features/powerlevel/Role.kt
@@ -1,7 +1,7 @@
 /*
- * Copyright 2025 New Vector Ltd.
+ * Copyright 2025 Progressive Chat
  *
- * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Progressive
  * Please see LICENSE files in the repository root for full details.
  */
 
